848ec5a4476728ffc2660e3e4559313a915f336e
vahid sk <vahidvadu098@gmail.com>
2026-02-06 21:17:47 +0530

    updated

--- app.js
+++ app.js
@@ -12,29 +12,23 @@ const { checkForAuthentication } = require('./middleware/auth')
 
 const app = express()
 
-
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(checkForAuthentication)
 
-//for ejs 
+// ejs
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
-//DBconnection
-const MONGO_URL = process.env.MONGO_ATLAS;
-connectionDB(MONGO_URL)
-
+// DB
+connectionDB(process.env.MONGO_ATLAS)
 
 // routes
-app.use('/', staticRoute) // home, landing pages
-app.use('/url', urlRoute) // url shortener
-app.use('/user', userRoute) // user routes
+app.use('/', staticRoute)
+app.use('/url', urlRoute)
+app.use('/user', userRoute)
 
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
-    console.log(`Server running at PORT ${PORT}`);
-})
+// ONLY export
+module.exports = app
